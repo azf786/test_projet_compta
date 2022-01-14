7f4f98e63740b6f842195a18c8ecae12871c9799
Azfar Mehmood <azfarmehmood998@gmail.com>
2022-01-14 15:29:00 +0100

    setup client server

---
+++ server-projet-compta/.eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "node": true,
+        "es6": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:vue/essential"
+    ],
+    "parserOptions": {
+        "ecmaVersion": 13,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "vue"
+    ],
+    "rules": {
+    }
+};
